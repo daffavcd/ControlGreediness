005ffde63b4e2d0e750713df0b9a8ebe315eb118
Muhammad Daffa' A.R <daffavcd@gmail.com>
2023-09-24 22:05:17 +0700

    layout history

--- lib/pages/history.dart
+++ lib/pages/history.dart
@@ -1,9 +1,11 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:loader_overlay/loader_overlay.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-
+import 'package:intl/intl.dart';
 import 'dashboard.dart';
+import 'package:flutter/services.dart';
 
 class History extends StatefulWidget {
   const History({super.key});
@@ -13,10 +15,31 @@ class History extends StatefulWidget {
 }
 
 class _HistoryState extends State<History> {
+  int historyCount = 3;
+  List history = [];
+
+  Future initialize() async {
+    // history = [];
+    // setState(() {
+    //   historyCount = history.length;
+    //   history = history;
+    // });
+    setState(() {
+      history = [];
+      historyCount = history.length;
+    });
+  }
+
+  @override
+  void initState() {
+    initialize();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return LoaderOverlay(
-      overlayColor: Color.fromARGB(73, 0, 170, 20),
+      overlayColor: const Color.fromARGB(73, 0, 170, 20),
       useDefaultLoading: false,
       overlayOpacity: 0.4,
       overlayWidget: const Center(
@@ -49,12 +72,82 @@ class _HistoryState extends State<History> {
                 ),
               ),
             ),
+            const SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 16.0),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: <Widget>[
+                      Card(
+                        elevation: 3.0, // Adjust the elevation as needed
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              10.0), // Adjust the border radius as needed
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 0, vertical: 8.0),
+                          child: ListTile(
+                            trailing: const CircleAvatar(
+                              backgroundColor: Color.fromARGB(255, 223, 11, 11),
+                              radius: 25,
+                              child: Icon(
+                                Icons.remove,
+                                color: Color.fromARGB(255, 255, 255, 255),
+                                size: 50.0,
+                              ), //CircleAvatar
+                            ),
+                            title: Text(
+                              "[-] Rp. 2500000",
+                              style: GoogleFonts.rubik(
+                                textStyle: const TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 23,
+                                ),
+                              ),
+                            ),
+                            subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  'Pembayaran Iuran kas',
+                                  style: GoogleFonts.rubik(
+                                    textStyle: const TextStyle(
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 18,
+                                      color: Colors.black87,
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  '24 September 2023',
+                                  style: GoogleFonts.rubik(
+                                    textStyle: const TextStyle(
+                                      fontWeight: FontWeight.w300,
+                                      fontSize: 17,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
             Padding(
               padding:
                   const EdgeInsets.symmetric(horizontal: 15, vertical: 16.0),
               child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 0, 170, 19),
+                  backgroundColor: Color.fromARGB(255, 3, 201, 26),
                   minimumSize: const Size.fromHeight(50), // NEW
                 ),
                 onPressed: () async {
